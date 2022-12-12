1e785d7031493c816835aa41166d8b9d576145df
KauanAraujoDev <kauanaraujo.dev@gmail.com>
2022-12-11 21:11:27 -0300

    Second Commit

--- src/components/Landing/AboutMe/index.tsx
+++ src/components/Landing/AboutMe/index.tsx
@@ -1,32 +1,40 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
 
 import './style.css'
-import { AboutMeButton } from './style'
-import curriculum from '../../../assets/curriculum.pdf'
+import { AboutMeButton } from './style';
+import curriculum from '../../../assets/curriculum.pdf';
+import UserPortfolio from '../../../assets/userportfolio.png'
 
-import { ReactComponent as Twitter } from '../../../assets/twitter.svg'
-import { ReactComponent as Twitch } from '../../../assets/twitch.svg'
-import { ReactComponent as Youtube } from '../../../assets/youtube.svg'
-import { ReactComponent as Figma } from '../../../assets/figma.svg'
-import { ReactComponent as CodePen } from '../../../assets/codePen.svg'
-import { ReactComponent as Facebook } from '../../../assets/facebook.svg'
+import { ReactComponent as Twitch } from '../../../assets/twitch.svg';
 
-import { ReactComponent as Download } from '../../../assets/download-cloud.svg'
-import { ReactComponent as Email } from '../../../assets/email.svg'
+import { ReactComponent as Download } from '../../../assets/download-cloud.svg';
+import { ReactComponent as Email } from '../../../assets/email.svg';
 
 export default function AboutMe() {
+    const ImageClient = styled.div`
+        width: 100%;
+        height: 100%;
+        border-radius: .25rem;
+        background: url(${UserPortfolio});
+        background-repeat: no-repeat;
+        background-size: 100% 100%;
+    `
+
     return (
         <React.Fragment>
             <div id='aboutme' className='aboutme-container'>
                 <div className='aboutme-container-div'>
                     <div className='aboutme-photo'>
                         <div className='aboutme-photo-bar'></div>
-                        <div className='aboutme-photo-image'></div>
+                        <div className='aboutme-photo-image'>
+                            <ImageClient />
+                        </div>
                     </div>
                     <div className='aboutme-text'>
                         <div className='text'>
                             <h3>SOBRE MIM</h3>
-                            <h4>São Paulo, Brasil</h4>
+                            <h4>São Geraldo Do Araguaia, Brasil</h4>
                             <p><small>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent id egestas dui. Nullam finibus aliquam enim quis faucibus. Aenean ac commodo dolor, nec bibendum velit.</small></p>
                         </div>
                         <div className='aboutme-svg-list'>
